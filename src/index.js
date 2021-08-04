537de89b4ea2dea2d8e9b4e01b7d98f023767cb4
Enzo Santilli <enzo135246mail.com>
2021-08-04 16:38:48 -0300

    Tercer subida

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const cors = require('cors')
 require('dotenv').config();
-
+const path = require('path');
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json())
