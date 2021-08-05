36a16006201574f2dbcfada4dfbffe0b7afcef29
Enzo Santilli <enzo135246mail.com>
2021-08-05 14:31:38 -0300

    Cors

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,45 +2,53 @@ const {Router} = require('express');
 const router = Router();
 const nodemailer = require('nodemailer')
 
-const sendMail = async (req, res)=>{
-    console.log("Email Enviado")
-    // const {name, email, subject, message} = req.body;
-    // console.log(req.body)
-    // contentHTML = `
-    //     <h1>Información de usuario</h1>
-    //     <ul>
-    //         <li>Nombre: ${name}</li>
-    //         <li>Email: ${email}</li>
-    //     </ul>
-    //     <br />
-    //     <br />
-    //     <p>
-    //     ${message}
-    //     </p>
-    // `
-    // let transporter= nodemailer.createTransport({
-    //     host: 'mail.enzosantilli.com.ar',
-    //     port: 465,
-    //     secure: true,
-    //     auth: {
-    //         user: 'enzosantilli@enzosantilli.com.ar',
-    //         pass: `${process.env.PASSWORD}`
-    //     },
-    //     tls: {
-    //         rejectUnauthorized: false
-    //     }
-    // });
+const sendMailFunction = async (req, res)=>{
+    const {name, email, subject, message} = req.body;
+    console.log(req.body)
+    contentHTML = `
+        <h1>Información de usuario</h1>
+        <ul>
+            <li>Nombre: ${name}</li>
+            <li>Email: ${email}</li>
+        </ul>
+        <br />
+        <br />
+        <p>
+        ${message}
+        </p>
+    `
+    let transporter= nodemailer.createTransport({
+        host: 'mail.enzosantilli.com.ar',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'enzosantilli@enzosantilli.com.ar',
+            pass: `${process.env.PASSWORD}`
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
 
-    // let info = await transporter.sendMail({
-    //     from: "'Enzo Santilli Server' <enzosantilli@enzosantilli.com.ar>",
-    //     to: 'enzo.santilli16@gmail.com',
-    //     subject: `${subject}`,
-    //     html: contentHTML
-    // });
+    let info = await transporter.sendMail({
+        from: "'Enzo Santilli Server' <enzosantilli@enzosantilli.com.ar>",
+        to: 'enzo.santilli16@gmail.com',
+        subject: `${subject}`,
+        html: contentHTML
+    });
+    transporter.sendMail(info, (error, data)=>{
+        if (error){
+            res.status(500).send(error.message)
+        }else{
+            console.log('Message sent', info.messageId);
+            res.status(200).json(req.body)
+        }
+    });
 
-    // console.log('Message sent', info.messageId);
+
+
 }
 
-router.post('/send-email', sendMail)
+router.post('/send-email', sendMailFunction)
 
 module.exports = router;
