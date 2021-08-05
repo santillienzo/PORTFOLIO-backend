ca3ecba77bcd908514dc8aa6ed029309ccc24369
Enzo Santilli <enzo135246mail.com>
2021-08-05 11:55:25 -0300

    Eliminé repo de git

--- src/index.js
+++ src/index.js
@@ -15,13 +15,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('port', process.env.PORT || 5000);
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
 
 app.listen(app.get('port'), ()=>{
     console.log(`Server on port ${app.get('port')}`)
