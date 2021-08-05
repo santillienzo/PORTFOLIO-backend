39eca95bfa62a71d381984052759a2eda2a1e0ed
Enzo Santilli <enzo135246mail.com>
2021-08-05 10:16:34 -0300

    Agregué función con headers

--- src/index.js
+++ src/index.js
@@ -14,7 +14,14 @@ app.use('/', require('./routes/index'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('port', process.env.PORT || 5000);
-app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 app.listen(app.get('port'), ()=>{
     console.log(`Server on port ${app.get('port')}`)
