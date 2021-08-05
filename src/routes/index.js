9b6231c943dc001fd7c21b9c14b354e7dc7ef2ba
Enzo Santilli <enzo135246mail.com>
2021-08-05 14:25:08 -0300

    Cors

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,41 +3,42 @@ const router = Router();
 const nodemailer = require('nodemailer')
 
 const sendMail = async (req, res)=>{
-    const {name, email, subject, message} = req.body;
-    console.log(req.body)
-    contentHTML = `
-        <h1>Información de usuario</h1>
-        <ul>
-            <li>Nombre: ${name}</li>
-            <li>Email: ${email}</li>
-        </ul>
-        <br />
-        <br />
-        <p>
-        ${message}
-        </p>
-    `
-    let transporter= nodemailer.createTransport({
-        host: 'mail.enzosantilli.com.ar',
-        port: 465,
-        secure: true,
-        auth: {
-            user: 'enzosantilli@enzosantilli.com.ar',
-            pass: `${process.env.PASSWORD}`
-        },
-        tls: {
-            rejectUnauthorized: false
-        }
-    });
+    console.log("Email Enviado")
+    // const {name, email, subject, message} = req.body;
+    // console.log(req.body)
+    // contentHTML = `
+    //     <h1>Información de usuario</h1>
+    //     <ul>
+    //         <li>Nombre: ${name}</li>
+    //         <li>Email: ${email}</li>
+    //     </ul>
+    //     <br />
+    //     <br />
+    //     <p>
+    //     ${message}
+    //     </p>
+    // `
+    // let transporter= nodemailer.createTransport({
+    //     host: 'mail.enzosantilli.com.ar',
+    //     port: 465,
+    //     secure: true,
+    //     auth: {
+    //         user: 'enzosantilli@enzosantilli.com.ar',
+    //         pass: `${process.env.PASSWORD}`
+    //     },
+    //     tls: {
+    //         rejectUnauthorized: false
+    //     }
+    // });
 
-    let info = await transporter.sendMail({
-        from: "'Enzo Santilli Server' <enzosantilli@enzosantilli.com.ar>",
-        to: 'enzo.santilli16@gmail.com',
-        subject: `${subject}`,
-        html: contentHTML
-    });
+    // let info = await transporter.sendMail({
+    //     from: "'Enzo Santilli Server' <enzosantilli@enzosantilli.com.ar>",
+    //     to: 'enzo.santilli16@gmail.com',
+    //     subject: `${subject}`,
+    //     html: contentHTML
+    // });
 
-    console.log('Message sent', info.messageId);
+    // console.log('Message sent', info.messageId);
 }
 
 router.post('/send-email', sendMail)
